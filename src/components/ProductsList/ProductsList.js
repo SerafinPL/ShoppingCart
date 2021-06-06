24d3b2c5566ae425170e50cbe4407cfd8b051042
serafinpl <ser.burberibar@gmail.com>
2021-06-06 15:22:38 +0200

    sass variables to color are added and fix bug with more than one product

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -9,12 +9,17 @@ import Product from "../Product/Product";
 const ProductsList = (props) => {
   let products = [];
   const productToShow = props.cart.forEach((element) => {
-    products = props.products.filter((item) => item.id === element.productId);
+    products.push(props.products.filter((item) => item.id === element.productId));
   });
 
-  console.log(products);
+  
+  const reducedProducts = products.reduce((a, b) => {
+    return a.concat(b);
+  });
+  console.log(reducedProducts)
 
-  return products.map((item) => {
+
+  return reducedProducts.map((item) => {
     const [qty] = props.cart.filter((element) => element.productId === item.id);
     return <Product key={item.id} value={item} qty={qty.quantity} />;
   });
